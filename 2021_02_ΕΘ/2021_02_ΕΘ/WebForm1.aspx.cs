493fc32c0ce3393ddbd121fb293b7d017e3ecce4
malvin-hoxha <hoxhamalvin25@gmail.com>
2025-02-14 11:57:59 +0200

    ASP.NET Web Service for Cube and Power Calculation Created an ASP.NET Web Service (_2020_02_ΕΘ) with two methods: THREE: Computes the cube of a given number. POW: Calculates the result of raising one number to the power of another. Developed an ASP.NET Web Forms page (Form.aspx) to interact with the Web Service: Button 1 calls the THREE method and displays the cube of the input number in Label1. Button 2 calls the POW method and displays the power calculation result in Label2. Configured the Web Service to be accessible from the Web Forms page. Added UI components including text boxes for input, buttons to trigger calculations, and labels to display results. Improved input validation and structured the code for better readability and usability.

---
+++ 2021_02_ΕΘ/2021_02_ΕΘ/WebForm1.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _2021_02_ΕΘ
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        WebService1 service1 = new WebService1();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            double number = service1.first(Convert.ToDouble(TextBox1.Text));
+            Label1.Text = number.ToString();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            double number = service1.second(Convert.ToDouble(TextBox2.Text), Convert.ToDouble(TextBox3.Text));
+            Label2.Text = number.ToString();
+        }
+    }
+}
---
+++ 2021_02_ΕΘ/2021_02_ΕΘ/WebService1.asmx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace _2021_02_ΕΘ
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public double first(double number)
+        {
+            return Math.Pow(number, 3);
+        }
+
+        public double second(double number, double number2)
+        {
+            return Math.Pow(number, number2);
+        }
+    }
+}
