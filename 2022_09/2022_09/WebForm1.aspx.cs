120d9252116e7fe507b78d29a46583247a8e53ca
malvin-hoxha <hoxhamalvin25@gmail.com>
2025-02-19 00:15:23 +0200

    ASP.NET Web Service for displaying football team coaches
    Created an ASP.NET Web Service (_2022_09) with methods to fetch team and coach information:
    
    TeamNames: Retrieves a list of all national football teams.
    AllTeamCoachNames: Retrieves the coaches of all national teams.
    Developed an ASP.NET Web Forms page that interacts with the Web Service:
    
    A dropdown (DropDownList1) allows users to select a national team.
    Upon selection, the DropDownList1_SelectedIndexChanged method retrieves the corresponding coach's name from the web service and displays it in Label2.
    Configured the Web Service to fetch and return team and coach data.
    
    Added UI elements like dropdowns and labels to facilitate user interaction and display coach names.

---
+++ 2022_09/2022_09/WebForm1.aspx.cs
@@ -0,0 +1,43 @@
+﻿using _2022_09.eu.dataaccess.footballpool.ws;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _2022_09
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        eu.dataaccess.footballpool.ws.Info fbWS = new eu.dataaccess.footballpool.ws.Info();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack == false)
+            {
+                string[] teamNames = fbWS.TeamNames();
+                DropDownList1.Items.Add("- Επιλέξτε Εθνική Ομάδα -");
+
+                for (int i=0; i < teamNames.Length;i++)
+                {
+                    DropDownList1.Items.Add(teamNames[i]);
+                }
+            }
+
+        }
+
+        
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            eu.dataaccess.footballpool.ws.tTeamCoachName[] coaches = fbWS.AllTeamCoachNames();
+            for (int i = 0; i < coaches.Length; i++)
+            {
+                if (DropDownList1.Text == coaches[i].sTeamName)
+                {
+                    Label2.Text = coaches[i].sCoachName;
+                }
+            }
+        }
+    }
+}
