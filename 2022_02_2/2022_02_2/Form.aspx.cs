507671d766d463bdac6e752d83f960d2b5b0f862
malvin-hoxha <hoxhamalvin25@gmail.com>
2025-02-18 23:41:43 +0200

    ASP.NET Web Form for displaying players properties
    Created an ASP.NET Web Service (_2022_02_2) that provides football player data:
    
    PlayersWithCardsRanked: Retrieves player names and their red card counts.
    AllPlayerNames: Retrieves player names and their associated countries.
    Developed an ASP.NET Web Forms page that interacts with the Web Service:
    
    The page dynamically displays the players with red cards along with their country names.
    The data is retrieved from the web service and displayed in the Label1 control.
    Configured the Web Service to return the necessary data for players with red cards and their corresponding countries.
    
    Added basic UI elements such as a label to display the output.

---
+++ 2022_02_2/2022_02_2/Form.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _2022_02_2
+{
+    public partial class Form : System.Web.UI.Page
+    {
+        eu.dataaccess.footballpool.ws.Info fbs = new eu.dataaccess.footballpool.ws.Info();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            eu.dataaccess.footballpool.ws.tTeamPlayerCardRankInfo[] TPCR = fbs.PlayersWithCardsRanked();
+            eu.dataaccess.footballpool.ws.tPlayerName[] TPN = fbs.AllPlayerNames(true);
+
+            for (int i = 0; i < TPCR.Length; i++)
+            {
+                if (TPCR[i].iRedCards>0)
+                {
+                    for (int j = 0; j < TPN.Length; j++)
+                    {
+                        if (TPCR[i].sName == TPN[j].sName)
+                        {
+                            Label1.Text = Label1.Text + "( " + TPCR[i].sName + " " + TPN[j].sCountryName + " ) ";
+                        }
+                    }
+                }
+            }
+
+        }
+
+    }
+}
---
+++ 2022_02_2/2022_02_2/WebForm1.aspx.cs
@@ -0,0 +1,42 @@
+﻿using _2022_02_2.eu.dataaccess.footballpool.ws;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _2022_02_2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        eu.dataaccess.footballpool.ws.Info fbWS = new eu.dataaccess.footballpool.ws.Info();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            eu.dataaccess.footballpool.ws.tTeamPlayerCardRankInfo[] can = fbWS.PlayersWithCardsRanked();
+            eu.dataaccess.footballpool.ws.tPlayerName[] cann;
+
+            for (int i = 0; i < can.Length; i++)
+            {
+
+                if (can[i].iRedCards > 0)
+                {
+                    cann = fbWS.AllPlayerNames(true);
+                    for (int j = 0; j < cann.Length; j++)
+                    {
+                        if (can[i].sName == cann[j].sName)
+                        {
+                            Label1.Text = Label1.Text + " (" + can[i].sName + " " + cann[j].sCountryName + ") ";
+                        }
+                    }
+                }
+
+                
+            }
+
+            
+
+
+        }
+    }
+}
