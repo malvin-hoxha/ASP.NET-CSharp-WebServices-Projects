c82a3d96252d044efec6344fd3c06cab9803bf6d
malvin-hoxha <hoxhamalvin25@gmail.com>
2025-02-24 22:08:20 +0200

    ASP.NET Web Application for Football Team Coaches and Flags
    - Developed an **ASP.NET Web Forms page** (_2023_02_2_) that interacts with the **Football Pool Web Service** to retrieve and display football team coaches and their corresponding national teams.
    
    - Implemented the following functionality:
      1. **DropDownList (DropDownList1)**: Populates dynamically with football team coaches from the web service.
      2. **Label (Label1)**: Displays the national team name corresponding to the selected coach.
      3. **Image (Image1)**: Shows the country flag of the team when a coach is selected.
    
    - Configured **AutoPostBack** on the DropDownList to update information dynamically upon selection.
    
    - Improved UI by structuring elements for seamless user interaction and clear data representation.

---
+++ 2023_02_2/2023_02_2/Form.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Form.aspx.cs" Inherits="_2023_02_2.Form" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:DropDownList ID="DropDownList1" runat="server" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" AutoPostBack="true"></asp:DropDownList><br />
+            <asp:Label ID="Label1" runat="server" Text=""></asp:Label><br />
+            <asp:Image ID="Image1" runat="server" Width="50px" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ 2023_02_2/2023_02_2/Form.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _2023_02_2
+{
+    public partial class Form : System.Web.UI.Page
+    {
+        eu.dataaccess.footballpool.ws.Info FB = new eu.dataaccess.footballpool.ws.Info();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack == false)
+            {
+                eu.dataaccess.footballpool.ws.tTeamCoachName[] coaches = FB.AllTeamCoachNames();
+                DropDownList1.Items.Add("- Επιλέξτε Προπονητή -");
+
+                for (int i = 0; i < coaches.Length; i++)
+                {
+                    DropDownList1.Items.Add(coaches[i].sCoachName);
+                }
+            }
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            eu.dataaccess.footballpool.ws.tTeamCoachName[] coaches = FB.AllTeamCoachNames();
+            eu.dataaccess.footballpool.ws.tPlayerName[] players = FB.AllPlayerNames(true);
+            for (int i = 0;i < coaches.Length; i++)
+            {
+                for (int j = 0; j < players.Length; j++)
+                {
+                    if (DropDownList1.Text == coaches[i].sCoachName && coaches[i].sTeamName == players[j].sCountryName)
+                    {
+                        Label1.Text = coaches[i].sTeamName;
+                        Image1.ImageUrl = players[j].sCountryFlag;
+
+                    }
+                }
+                
+            }
+        }
+    }
+}
