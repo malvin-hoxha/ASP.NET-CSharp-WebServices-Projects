0c6b3def43eb78b558c2f618f0efcbb17b459a15
malvin-hoxha <hoxhamalvin25@gmail.com>
2025-02-26 12:41:59 +0200

    ASP.NET Web Service for Integer Division and Middle Word Extraction
    - Created an ASP.NET Web Service (_2023_06_) with two methods:
      1. **intDivision**: Computes the quotient and remainder of integer division.
      2. **MiddleWord**: Extracts the middle word from a given sentence.
    
    - Developed an ASP.NET Web Forms page that interacts with the Web Service:
      1. **Button 1** calls the `intDivision` method and displays the quotient and remainder in `Label1`.
      2. **Button 2** calls the `MiddleWord` method and displays the extracted middle word in `Label3`.
    
    - Configured the Web Service to allow interaction from web forms.
    - Added basic UI elements like text boxes, labels, and buttons for user input and output display.

---
+++ 2023_06/2023_06/WebForm1.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _2023_06
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        WebService1 service1 = new WebService1 ();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Label1.Text = service1.intDivision(Convert.ToInt32(TextBox1.Text), Convert.ToInt32(TextBox2.Text)).ToString();
+            
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Label3.Text = service1.MiddleWord(TextBox3.Text);
+        }
+    }
+}
---
+++ 2023_06/2023_06/WebService1.asmx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace _2023_06
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+        
+        [WebMethod]
+        public (int result1, int result2) intDivision(int num1, int num2)
+        {
+            
+            int result2;
+            result2 =  Math.DivRem(num1, num2, out int result1);
+            return (result1,result2);   
+        }
+
+        public String MiddleWord(String word)
+        {
+            int firstSpaceIndex = word.IndexOf(' ');
+            string sentence = word.Remove(0, firstSpaceIndex + 1);
+            int secondSpaceIndex = sentence.IndexOf(' ');
+            string middleWord = sentence.Remove(secondSpaceIndex);
+
+            return middleWord;
+        }
+    }
+}
